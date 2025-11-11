4abcdff2c2e640b523bb4746c4f35d0855e7f864
Luqmaan-io <luqmaan-123-@outlook.com>
2025-11-11 19:31:15 +0000

    add django files and configure settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gym-membership-tracker.wsgi
--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^4u_!n$2ds!i=huo6+5n9sjpdo5!no6m&nz!+!6gl#853&+4(q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 '127.0.0.1',
+]
 
 
 # Application definition
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hello_world',
+    'about',
 ]
 
 MIDDLEWARE = [
