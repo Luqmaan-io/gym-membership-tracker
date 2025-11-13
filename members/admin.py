1de5ea530ede39e839dfec90eb35d5c930754495
Luqmaan-io <luqmaan-123-@outlook.com>
2025-11-13 00:56:50 +0000

    revert changes to member/admin.py

--- members/admin.py
+++ members/admin.py
@@ -1,14 +1,5 @@
 from django.contrib import admin
-from .models import Gym
+from .models import Member
 
-class MemberInline(admin.TabularInline):
-    from members.models import Member
-    model = Member
-    extra = 0
-    readonly_fields = ['first_name', 'last_name', 'email', 'phone_number', 'status']
-    can_delete = False
-
-class GymAdmin(admin.ModelAdmin):
-    inlines = [MemberInline]
-
-admin.site.register(Gym, GymAdmin)
+# Register your models here.
+admin.site.register(Member)
