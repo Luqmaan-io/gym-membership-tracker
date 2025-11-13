b05f597ce7171803d88b3af88db529a5185271b8
Luqmaan-io <luqmaan-123-@outlook.com>
2025-11-13 00:53:21 +0000

    fix members/admin.py

--- members/admin.py
+++ members/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Member
+from .models import Gym
 
-# Register your models here.
-admin.site.register(Member)
+class MemberInline(admin.TabularInline):
+    from members.models import Member
+    model = Member
+    extra = 0
+    readonly_fields = ['first_name', 'last_name', 'email', 'phone_number', 'status']
+    can_delete = False
+
+class GymAdmin(admin.ModelAdmin):
+    inlines = [MemberInline]
+
+admin.site.register(Gym, GymAdmin)
