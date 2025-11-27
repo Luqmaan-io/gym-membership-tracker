a3021a1f48fce1a09ae84936daedc17c671919db
Luqmaan-io <luqmaan-123-@outlook.com>
2025-11-27 16:53:11 +0000

    save all changes to last commit when creating login page

--- members/views.py
+++ members/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from django.http import HttpResponse
 
-def members(request):
-    return HttpResponse("Members page coming soon!")
+
+def custom_login(request):
+    # If user is already logged in, redirect them
+    if request.user.is_authenticated:
+        return redirect('home')  # Redirect to homepage
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            # Login the user
+            login(request, user)
+            messages.success(request, f'Welcome back, {username}!')
+            return redirect('home')  # Redirect to homepage after login
+        else:
+            messages.error(request, 'Invalid username or password. Please try again.')
+    
+    # If GET request or failed login, show the login form
+    return render(request, 'members/login.html')
